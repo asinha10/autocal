95d3e35d50138b3da551e48d952bd4cc14d6c2a3
asinha10 <aakashtheaks@gmail.com>
2021-06-27 23:21:07 -0400

    Autocal v1.1 - 27th June 2021

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CalendarApp',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='aakash',
+    author_email='aakashtheaks@gmail.com',
+    description='AutoCal App'
+)
